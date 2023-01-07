57e519ece25354ac79ba6dbd2723b1626f537933
Aaron Kagan <aaronkagandev@gmail.com>
2023-01-07 01:45:27 -0500

    todos component skeleton

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import styled from 'styled-components';
 import Counter from './Counter';
+import Todos from './Todos';
 const App = () => {
   return (
     <Wrapper>
       <h3>First Example: Simple counter with one state</h3>
       <Counter />
       <h3>Second Example: Todo list with more complex state</h3>
+      <Todos />
     </Wrapper>
   );
 };
---
+++ src/Todos.js
@@ -0,0 +1,8 @@
+import { useReducer } from 'react';
+import styled from 'styled-components';
+
+const Todos = () => {
+  return <h1>Todos</h1>;
+};
+
+export default Todos;
