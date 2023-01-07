7febb9c57d88f27e6c684c82f7c5294f37daefdb
Aaron Kagan <aaronkagandev@gmail.com>
2023-01-07 01:37:57 -0500

    adding title

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ const App = () => {
     <Wrapper>
       <h3>First Example: Simple counter with one state</h3>
       <Counter />
+      <h3>Second Example: Todo list with more complex state</h3>
     </Wrapper>
   );
 };
