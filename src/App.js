5090ee67fdeefbe0a1c3c6e2147a8c25edaa0324
Aaron Kagan <aaronkagandev@gmail.com>
2023-01-07 00:49:23 -0500

    setting basic counter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
+import { useState } from 'react';
+import styled from 'styled-components';
 const App = () => {
-  return <div></div>;
+  const [count, setCount] = useState(0);
+
+  const incrementCount = () => {
+    setCount((prevCount) => prevCount + 1);
+  };
+
+  const decrementCount = () => {
+    setCount((prevCount) => prevCount - 1);
+  };
+
+  return (
+    <Wrapper>
+      <button onClick={decrementCount}>-</button>
+      <p>{count}</p>
+      <button onClick={incrementCount}>+</button>
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.div`
+  display: flex;
+  gap: 30px;
+  align-items: center;
+  padding-left: 5%;
+`;
 
 export default App;
