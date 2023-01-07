ff9585a9aa29d0082363934529a81ec588e6cf71
Aaron Kagan <aaronkagandev@gmail.com>
2023-01-07 01:48:31 -0500

    reducer skeleton

--- src/Todos.js
+++ src/Todos.js
@@ -2,7 +2,17 @@ import { useReducer } from 'react';
 import styled from 'styled-components';
 
 const Todos = () => {
-  return <h1>Todos</h1>;
+  const ACTIONS = {
+    ADD: 'addTodo',
+    DELETE: 'deleteTodo'
+  };
+
+  const reducer = (state, action) => {};
+
+  const initialState = [];
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  return <></>;
 };
 
 export default Todos;
